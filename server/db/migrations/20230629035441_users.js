e4b6b0c78008031e4664551d2ae92cba026a209c
Chris Swan <chrisswan1612@gmail.com>
2023-06-29 16:19:39 +1200

    first two migrations and updated documenation

--- server/db/migrations/20230629035441_users.js
+++ server/db/migrations/20230629035441_users.js
@@ -2,33 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
 exports.up = function (knex) {
-  return knex.schema
-    .createTable('users', function (table) {
-      table.increments('id').primary()
-      table.timestamp('created_at').defaultTo(Date.now())
-      table.string('name')
-      table.string('profile_image').defaultTo(null)
-      table.boolean('previous_winner').defaultTo(false)
-      table.boolean('is_deleted').defaultTo(false)
-    })
-    .then(() => {
-      return knex.schema.alterTable('users', function (table) {
-        table.index('name') // Index the name column
-      })
-    })
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id').primary()
+    table.timestamp('created_at').defaultTo(Date.now())
+    table.string('name')
+    table.string('profile_image').defaultTo(null)
+    table.string('email')
+  })
 }
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function (knex) {
-  return knex.schema
-    .alterTable('users', (table) => {
-      table.dropIndex('name') // Drop the index from name
-    })
-    .then(() => {
-      return knex.schema.dropTable('users')
-    })
+  return knex.schemaknex.dropTable('users')
 }
--- server/db/migrations/20230629035449_disputes.js
+++ server/db/migrations/20230629035449_disputes.js
@@ -2,14 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  knex.schema.createTable('disputes', function (table) {
+    table.increments('id').primary()
+    table.timestamp('created_at').defaultTo(Date.now())
+    table.integer('user_id')
+    table.integer('infringement')
+    table.string('registration')
+    table.date('date_issued')
+    table.string('time_issued')
+    table.string('location')
+    table.integer('amount')
+    table.string('status').defaultTo('New')
+  })
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {}
