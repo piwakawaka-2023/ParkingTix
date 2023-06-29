f67ca41f787a316792a7b4cb6f6fb1ed0fbe6290
Chris Swan <chrisswan1612@gmail.com>
2023-06-29 14:59:33 +1200

    add models documentation

---
+++ models/users.ts
@@ -0,0 +1,20 @@
+export interface UserObj {
+  id: number
+  name: string
+  created_at: number
+  profile_image: string | null
+  email: string
+}
+
+export interface New {
+  name: string
+  profile_image?: string
+  email: string
+}
+
+export interface Update {
+  name?: string
+  profile_image?: string
+  email?: string
+}
+
