51e78ad20229c783cc6c90bd0842b8c32b2d4221
chris-swan <93796699+Chrisp-Swanch@users.noreply.github.com>
2023-07-03 09:54:42 +1200

    update server side route path

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import users from './routes/users'
 import disputes from './routes/disputes'
 import emails from './routes/emails'
-import router from './routes/openAiRoutes'
+import openai from './routes/openai'
 
 const server = express()
 
@@ -13,6 +13,6 @@ server.use(express.static(path.join(__dirname, 'public')))
 server.use('/api/v1/users', users)
 server.use('/api/v1/disputes', disputes)
 server.use('/api/v1/emails', emails)
-server.use('/api/v1/openAI', router)
+server.use('/api/v1/openai', openai)
 
 export default server
