8ecf8a6daf9d7388beddf8872a390920cee3acbe
chris-swan <93796699+Chrisp-Swanch@users.noreply.github.com>
2023-07-03 16:19:09 +1200

    set up test email component

--- client/apis/users.ts
+++ client/apis/users.ts
@@ -9,47 +9,29 @@ export async function fetchAllUsers(): Promise<UserModels.UserObj> {
   return users
 }
 
-export async function fetchUser(
-  userId: number,
-  token: string //auth
-): Promise<UserModels.UserObj> {
-  const res = await request
-    .get(`${userUrl}/${userId}`)
-    .set('Authorization', `Bearer${token}`) //auth
+export async function fetchUser(userId: number): Promise<UserModels.UserObj> {
+  const res = await request.get(`${userUrl}/${userId}`)
   const user = res.body
   return user
 }
 
 export async function postUser(
-  newUser: UserModels.New,
-  token: string //auth
+  newUser: UserModels.New
 ): Promise<UserModels.UserObj> {
-  const res = await request
-    .post(userUrl)
-    .set('Authorization', `Bearer${token}`) //auth
-    .send(newUser)
+  const res = await request.post(userUrl).send(newUser)
   const newUserFromDb = res.body
   return newUserFromDb
 }
 
 export async function patchUser(
   id: number,
-  newUser: UserModels.Update,
-  token: string //auth
+  newUser: UserModels.Update
 ): Promise<UserModels.UserObj> {
-  const res = await request
-    .patch(`${userUrl}/${id}`)
-    .set('Authorization', `Bearer${token}`) //auth
-    .send({ user: newUser })
+  const res = await request.patch(`${userUrl}/${id}`).send({ user: newUser })
   const newUserFromDb = res.body
   return newUserFromDb
 }
 
-export async function deleteUser(
-  id: number,
-  toke: string //auth
-) {
-  await request
-    .delete(`${userUrl}/${id}`)
-    .set('Authorization', `Bearer${token}`) //auth
+export async function deleteUser(id: number) {
+  await request.delete(`${userUrl}/${id}`)
 }
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -7,12 +7,12 @@ import Header from './Header'
 import Home from './Home'
 import Nav from './Nav'
 import { CssBaseline } from '@mui/material'
+import EmailTest from './EmailTest'
 
 // Temp/hardcoded user id of 1. This will change when we get Auth0 set up
 export const userId = 1
 
 function App() {
-  
   // Authenticated/Not Authenticated thingies go in this component
   // Db called to get user data from Home component once authenticated
 
@@ -30,6 +30,7 @@ function App() {
           <Route path="/signup" element={<AddUser />} />
           <Route path="/disputes" element={<DisputesList />} />
           <Route path="/disputes/add" element={<AddDispute />} />
+          <Route path="/test" element={<EmailTest />} />
         </Routes>
       </section>
     </>
