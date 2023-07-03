7a1273374ab8c31f0d2e9d60d7ea3c6d6f86b4e8
Callum Watson <cbwatson23@gmail.com>
2023-07-03 15:52:16 +1200

    Home page complete.

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -7,12 +7,13 @@ import Header from './Header'
 import Home from './Home'
 import Nav from './Nav'
 import { CssBaseline } from '@mui/material'
+import Footer from './Footer'
+import NotFoundPage from './NotFoundPage'
 
 // Temp/hardcoded user id of 1. This will change when we get Auth0 set up
 export const userId = 1
 
 function App() {
-  
   // Authenticated/Not Authenticated thingies go in this component
   // Db called to get user data from Home component once authenticated
 
@@ -30,7 +31,9 @@ function App() {
           <Route path="/signup" element={<AddUser />} />
           <Route path="/disputes" element={<DisputesList />} />
           <Route path="/disputes/add" element={<AddDispute />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
+        <Footer />
       </section>
     </>
   )
