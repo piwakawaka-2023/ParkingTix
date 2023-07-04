b9bcb93a1b9af85b2100995fd3341f5943474a2a
Ektaj Singh <ektaj@outlook.com>
2023-07-04 16:09:52 +1200

    resolved and failed buttons work

--- client/components/Dispute.tsx
+++ client/components/Dispute.tsx
@@ -1,9 +1,11 @@
 // import { useState } from 'react'
+import { useState } from 'react'
 import * as DisputeModels from '../../models/disputes'
 import * as action from '../actions/disputes'
 import { useAppDispatch } from '../hooks/hooks'
 import { ThunkAction } from '../store'
 import EmailsList from './EmailsList'
+import { update } from 'immutable'
 
 interface Props {
   dispute: DisputeModels.DisputeObj
@@ -20,6 +22,7 @@ function Dispute(props: Props) {
     time_issued,
     location,
     amount,
+    status,
   } = dispute
   // const [isSelected, setIsSelected] = useState(false)
 
@@ -27,6 +30,16 @@ function Dispute(props: Props) {
 
   const handleDelete = (id: number) => {
     dispatch(action.deleteDisputeThunk(id))
+  }
+
+  const handleResolved = () => {
+    dispatch(action.updateDisputeThunk(id, { status: 'Resolved' }))
+    window.location.reload()
+  }
+
+  const handleFailed = () => {
+    dispatch(action.updateDisputeThunk(id, { status: 'Failed' }))
+    window.location.reload()
   }
 
   return (
@@ -40,14 +53,15 @@ function Dispute(props: Props) {
           <p>Location:{location}</p>
           <p>Amount Due: ${amount}</p>
         </div>
+        <p>{status}</p>
         <EmailsList key={id} disputeId={id} />
       </div>
       {/* STYLING FOR DROP DOWN IS IN ./STYLES/INDEX.CSS */}
       <div className="dropdown">
         <button>Change Status</button>
         <div className="dropdown-options">
-          <a href="#">Dispute Resolved</a>
-          <a href="#">Appeal Failed</a>
+          <a onClick={handleResolved}>Dispute Resolved</a>
+          <a onClick={handleFailed}>Appeal Failed</a>
           <button onClick={() => handleDelete(dispute.id)}>Delete</button>
         </div>
       </div>
